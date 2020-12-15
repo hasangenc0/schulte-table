7ee16b81a21ec84edbadeb53ff50633b93aa0640
Hasan Genc <hasan.genc@trendyol.com>
2020-12-15 09:14:29 +0300

    Change schulte table context

--- lib/context/sculte_table_context.dart
+++ lib/context/sculte_table_context.dart
@@ -2,29 +2,39 @@ import 'package:flutter/foundation.dart';
 
 class SchulteTableCellState {
   final int value;
-  bool visible = true;
-  bool showValue = true;
-  bool focused = true;
+  bool visible;
+  bool showValue;
+  bool focused;
+  bool popped;
+  Duration selectDuration;
 
-  SchulteTableCellState(this.value, this.visible, this.showValue, this.focused);
+  SchulteTableCellState(
+      this.value, this.visible, this.showValue, this.focused, this.popped);
 }
 
 class SchulteTableContext extends ChangeNotifier {
   Map<int, SchulteTableCellState> _items = new Map();
+  int _nextValue = 1;
 
   Map<int, SchulteTableCellState> get items => _items;
+  int get nextValue => _nextValue;
 
   SchulteTableContext(List<int> items) {
     items.forEach((element) {
-      _items[element] = SchulteTableCellState(element, true, true, true);
+      _items[element] = SchulteTableCellState(element, true, true, true, false);
     });
   }
 
   set itemState(SchulteTableCellState state) {
     _items.update(
         state.value,
-        (item) => SchulteTableCellState(
-            item.value, state.visible, state.showValue, state.focused));
+        (item) => SchulteTableCellState(item.value, state.visible,
+            state.showValue, state.focused, state.popped));
+    notifyListeners();
+  }
+
+  set nextValue(int value) {
+    _nextValue = value;
     notifyListeners();
   }
 }
