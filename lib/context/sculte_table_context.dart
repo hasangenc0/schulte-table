482ebaaee6ba1954c7ec7d64ccd89e5f34bd533d
Hasan Genc <hasan.genc@trendyol.com>
2020-12-17 00:08:58 +0300

    Chane table context

--- lib/context/sculte_table_context.dart
+++ lib/context/sculte_table_context.dart
@@ -6,18 +6,26 @@ class SchulteTableCellState {
   bool showValue;
   bool focused;
   bool popped;
+  bool inAction;
   Duration selectDuration;
 
   SchulteTableCellState(
-      this.value, this.visible, this.showValue, this.focused, this.popped);
+      this.value, this.visible, this.showValue, this.focused, this.popped,
+      [this.inAction = false]);
 }
 
 class SchulteTableContext extends ChangeNotifier {
   Map<int, SchulteTableCellState> _items = new Map();
   int _nextValue = 1;
+  bool _started = false;
+  bool _finished = false;
+  Duration _score;
 
   Map<int, SchulteTableCellState> get items => _items;
   int get nextValue => _nextValue;
+  bool get started => _started;
+  bool get finished => _finished;
+  Duration get score => _score;
 
   SchulteTableContext(List<int> items) {
     items.forEach((element) {
@@ -26,15 +34,27 @@ class SchulteTableContext extends ChangeNotifier {
   }
 
   set itemState(SchulteTableCellState state) {
-    _items.update(
-        state.value,
-        (item) => SchulteTableCellState(item.value, state.visible,
-            state.showValue, state.focused, state.popped));
+    _items.update(state.value, (item) => state);
     notifyListeners();
   }
 
   set nextValue(int value) {
     _nextValue = value;
+    notifyListeners();
+  }
+
+  set started(bool isStarted) {
+    _started = isStarted;
+    notifyListeners();
+  }
+
+  set finished(bool isFinished) {
+    _finished = isFinished;
+    notifyListeners();
+  }
+
+  set score(Duration score) {
+    _score = score;
     notifyListeners();
   }
 }
